0bcaa21c66a212f815c4f389684acb7cfca21a81
rayzi0112 <rayziginting4@gmail.com>
2025-05-24 00:22:03 +0700

    Feat: add route test

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Api backedn for box by Telecomunication engineering 21");
+});
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api", require("./routes/boxRoutes"));
 
