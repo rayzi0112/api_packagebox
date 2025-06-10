75b0f248294fb811fc5cbcc68d6a0ed9164e802c
rayzi0112 <rayziginting4@gmail.com>
2025-06-11 00:03:13 +0700

    FeatL Update endpoint

--- routes/fcmTokenRoutes.js
+++ routes/fcmTokenRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { registerToken, getToken } = require("../controllers/fcmTokenController");
 
-router.post("/fcm-tokens", registerToken);
+router.post("/fcm-tokens/register", registerToken);
 router.get("/fcm-tokens/token", getToken);
 
 module.exports = router;
